2674e6c30bffc7a788f37a3a5ac63d8ce307e317
Alena Rebkavets <archirebkavets@gmail.com>
2020-02-26 12:35:05 +0300

    feat add file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+
+
+    if (array.length !== 0 || array !== undefined)
+        return array.sort((a, b) => b - a).pop();
+
+    return 0;
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let a = array.sort((a, b) => a - b);
+    let result = a.pop();
+    if (array.length > 1)
+        return result;
+
+    if (array.length === 0 || array === undefined)
+        return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    let result = array.reduce((sum, current) => sum + current, 0);
+    let result2 = result / array.length;
+    if (array.length > 1)
+        return result2;
+
+    if (array.length === 0 || array === undefined)
+        return 0;
 }
